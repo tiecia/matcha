ac1b700d5e14f7058635de654da769259da06a8c
QuincePie <flaky@quincepie.dev>
2023-07-20 19:33:39 -0500

    Added shared memory for destroying the enhibitor and recreating it

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include "idle-inhibit-unstable-v1.h"
-#include "sys/types.h"
-#include "wayland-client-core.h"
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <wayland-client.h>
+
+#define SHARED_MEM_NAME "/matcha-idle-inhibit"
 
 typedef struct wl_registry wl_registry;
 typedef struct wl_compositor wl_compositor;
@@ -15,6 +18,7 @@ typedef struct zwp_idle_inhibitor_v1 zwp_idle_inhibitor_v1;
 typedef struct wl_display wl_display;
 
 typedef struct {
+    bool* inhibit;
     wl_registry* registry;
     wl_display* display;
     wl_compositor* compositor;
@@ -64,6 +68,7 @@ static void global_registry_handler(void* data, struct wl_registry* registry, ui
 
 static void global_registry_remover(void* data, struct wl_registry* registry, uint32_t id) {
     // do nothing
+    printf("Removing %d\n", id);
     (void)data;
     (void)registry;
     (void)id;
@@ -72,7 +77,54 @@ static void global_registry_remover(void* data, struct wl_registry* registry, ui
 static const struct wl_registry_listener registry_listener = {global_registry_handler,
                                                               global_registry_remover};
 
-int main(void) {
+// create shared memory
+static bool* create_shared_mem() {
+    int shm_fd = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0660);
+    if (shm_fd == -1) {
+        perror("Failed to create shared memory");
+        exit(1);
+    }
+    if (ftruncate(shm_fd, sizeof(bool)) == -1) {
+        perror("Failed to truncate shared memory");
+        exit(1);
+    }
+    bool* data = (bool*)mmap(0, sizeof(bool), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (data == MAP_FAILED) {
+        perror("Failed to map shared memory");
+        exit(1);
+    }
+    close(shm_fd);
+    *data = true;
+    return data;
+}
+
+static bool* access_shared_mem() {
+    int shm_fd = shm_open(SHARED_MEM_NAME, O_RDWR, 0660);
+    if (shm_fd == -1) {
+        perror("Failed to access shared memory");
+        exit(1);
+    }
+
+    bool* data = (bool*)mmap(0, sizeof(bool), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (data == MAP_FAILED) {
+        perror("Failed to map shared memory");
+        exit(1);
+    }
+    close(shm_fd);
+    return data;
+}
+
+int main(int argc, char** argv) {
+    // toggle is used for toggling the state of the inhibitor
+    if (argc == 2 && strcmp(argv[1], "--toggle") == 0) {
+        bool* inhibit = access_shared_mem();
+        *inhibit = !*inhibit;
+        return EXIT_SUCCESS;
+    } else if (argc != 1) {
+        fprintf(stderr, "Usage: %s [--toggle]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     MatchaBackend* backend = (MatchaBackend*)malloc(sizeof(MatchaBackend));
     if (!backend) {
         fprintf(stderr, "Failed to allocate memory for backend\n");
@@ -104,20 +156,27 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    backend->idle_inhibitor = zwp_idle_inhibit_manager_v1_create_inhibitor(
-        backend->idle_inhibit_manager, backend->surface);
+    backend->inhibit = create_shared_mem();
 
-    if (!backend->idle_inhibitor) {
-        fprintf(stderr, "Failed to create inhibitor\n");
-        matcha_destroy(backend);
-        return EXIT_FAILURE;
-    }
-
-    wl_surface_commit(backend->surface);
-    wl_display_roundtrip(backend->display);
-
-    while (wl_display_dispatch(backend->display) != -1) {
-        // do nothing
+    while (1) {
+        if (!*backend->inhibit) {
+            if (backend->idle_inhibitor) {
+                printf("Pausing Matcha\n");
+                zwp_idle_inhibitor_v1_destroy(backend->idle_inhibitor);
+                wl_surface_commit(backend->surface);
+                wl_display_roundtrip(backend->display);
+                backend->idle_inhibitor = NULL;
+            }
+        } else {
+            if (!backend->idle_inhibitor) {
+                printf("Starting Matcha\n");
+                backend->idle_inhibitor = zwp_idle_inhibit_manager_v1_create_inhibitor(
+                    backend->idle_inhibit_manager, backend->surface);
+                wl_surface_commit(backend->surface);
+                wl_display_roundtrip(backend->display);
+            }
+        }
+        sleep(1);
     }
 
     matcha_destroy(backend);
